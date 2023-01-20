d1637a79f7e22c636ce4189c92d368533ef8a9d6
mwaq <maham.waqar1@gmail.com>
2023-01-20 15:05:21 -0500

    Finished assignment 1

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+/*
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy. 
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+*
+*  Name: Maham Waqar Student ID: 127044196 Date: 2023-01-20
+*
+*  Online (Cyclic) URL: https://curious-bat-pinafore.cyclic.app
+/*
+**/
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Maham Waqar - 127044196");
 });
 
 // setup http server to listen on HTTP_PORT
